7ab3a81a5ca8a79bad2adaa169a052760b4c2ee8
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-18 16:20:47 +0330

    commit

--- src/app/components/ParentListtTasks.jsx
+++ src/app/components/ParentListtTasks.jsx
@@ -4,17 +4,22 @@ export default function ParentListtTasks() {
   return (
     <div className="flex flex-col items-center">
       <div className="text-center pt-5">
-        <p>TO-DO Liast</p>
+        <p className="font-mono ">TO-DO Liast</p>
       </div>
       {/*  */}
       <div className="flex items-center ">
-        <button className="bg-purple-500 p-2 w-52  rounded-2xl ">
+        <button className="bg-purple-500 p-2 w-52  rounded-xl mb-10 ">
           <p>Add new task</p>
         </button>
       </div>
       <div className="flex flex-col">
         <ul>
-          <li></li>
+          <li><button className=" mt-6 "><p className="font-mono ">Today's tasks</p></button></li>
+          <li><button className=" mt-6"><p className="font-mono ">Important tasks</p></button></li>
+          <li><button className=" mt-6"><p className="font-mono ">Complated tasks</p></button></li>
+          <li><button className=" mt-6"><p className="font-mono ">Uncomplated tasks</p></button></li>
+         
+          
         </ul>
       </div>
     </div>
