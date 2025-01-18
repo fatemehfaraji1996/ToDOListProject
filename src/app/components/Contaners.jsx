dfdcc8235b4af9dc6ff68ad094abea37575096c0
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-18 12:10:12 +0330

    commit

---
+++ src/app/components/Contaners.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Contaners () {
+  return (
+    <div className=" flex flex-row h-96  ">
+      <div className=" w-2/12 bg-green-500">left</div>
+      <div className="bg-gray-50 w-8/12  ">center</div>
+      <div className="w-2/12 bg-gray-300">left</div>
+    </div>
+  );
+}
