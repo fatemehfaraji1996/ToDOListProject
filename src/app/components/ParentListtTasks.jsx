9f54000508cabede6d1f8a39e21388b457388f3b
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-24 20:50:45 +0330

    commit

--- src/app/components/ParentListtTasks.jsx
+++ src/app/components/ParentListtTasks.jsx
@@ -75,7 +75,7 @@ export default function ParentListtTasks() {
           className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
         >
           <ul
-            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+            className="py-2 text-sm text-gray-900 dark:text-gray-200"
             aria-labelledby="dropdownDelayButton"
           >
             <li>
