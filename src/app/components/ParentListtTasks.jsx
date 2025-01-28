8a5b9c31aa2e724b106114997f290db4ac628537
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-28 20:58:53 +0330

    droup down

--- src/app/components/ParentListtTasks.jsx
+++ src/app/components/ParentListtTasks.jsx
@@ -1,6 +1,10 @@
+'use client'
 import React from "react";
-
+import { useState } from 'react';
 export default function ParentListtTasks() {
+  const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <div className="flex flex-col items-center">
       <div className="text-center pt-5 mb-10">
@@ -14,88 +18,90 @@ export default function ParentListtTasks() {
       </div>
       {/*  */}
       <div className="flex flex-col w-full">
-        <div className="hover:bg-red-100 w-full   hover:border-r-4 border-red-500 hover:rounded-sm ">
-          <button className=" mt-6 hover:bg-red-100 ">
+        <div className="hover:bg-red-100 w-full   hover:border-r-4 border-blue-600 hover:rounded-sm ">
+          <button className=" mt-6 hover:bg-blue-100 ">
             <p className="font-mono pb-3 pl-2 ">Today's tasks</p>
           </button>
         </div>
 
-        <div className="hover:bg-red-100 w-full   hover:border-r-4 border-red-500 hover:rounded-sm ">
+        <div className="hover:bg-red-100 w-full   hover:border-r-4 border-purple-600 hover:rounded-sm ">
           <button className=" mt-6">
-            <p className="font-mono pb-3 pl-2">Important tasks</p>
+            <p className="font-mono pb-3 pl-2 bg-purple-300">Important tasks</p>
           </button>
         </div>
 
         <div className="hover:bg-red-100 w-full   hover:border-r-4 border-red-500 hover:rounded-sm ">
           <button className=" mt-6">
-            <p className="font-mono pb-3 pl-2">Complated tasks</p>
+            <p className="font-mono pb-3 pl-2 bg-red-500">Complated tasks</p>
           </button>
         </div>
 
         <div className="hover:bg-red-100 w-full   hover:border-r-4 border-red-500 hover:rounded-sm ">
           <button className=" mt-6">
-            <p className="font-mono pb-3 pl-2">Uncomplated tasks</p>
+            <p className="font-mono pb-3 pl-2 bg-green-300 ">Uncomplated tasks</p>
           </button>
         </div>
 
-        {/* <div className="hover:bg-red-100 w-full   hover:border-r-4 border-red-500 hover:rounded-sm ">
-          <button className=" mt-6">
-            <p className="font-mono pb-3 pl-2">Directoreis</p>
-          </button>
-        </div> */}
 
-        <button
-          id="dropdownDelayButton"
-          data-dropdown-toggle="dropdownDelay"
-          data-dropdown-delay="500"
-          data-dropdown-trigger="hover"
-          
-          className="    focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-          type="button"
+
+       
+
+
+
+
+{/* drop down menu */}
+    <div className="relative inline-block text-left">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="inline-flex justify-center w-full rounded-md border shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Options
+        <svg
+          className="-mr-1 ml-2 h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
         >
-          Directoreis{" "}
-          <svg
-            className="w-2.5 h-2.5 ms-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
-        </button>
-        <div
-          id="dropdownDelay"
-          className="z-10 hidden bg-white divide-y divide-gray-900 rounded-lg shadow w-44 dark:bg-gray-700"
-        >
-          <ul
-            className="py-2 text-sm text-gray-100 dark:text-gray-200"
-            aria-labelledby="dropdownDelayButton"
-          >
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Settings
-              </a>
-            </li>
-          </ul>
+          <path
+            fillRule="evenodd"
+            d="M10 3a1 1 0 01.707 1.707l-3 3a1 1 0 01-1.414 0l-3-3A1 1 0 016 3h8z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div className="py-1">
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+         
+              <p>Todos</p>
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Option 2
+            </a>
+       
+          </div>
         </div>
+      )}
+    </div>
+
+
+
+
+
+
+
+
+
+
       </div>
     </div>
   );
