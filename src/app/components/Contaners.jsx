6f25fb17b96ace43493cd8ea16cbe45d51de7fbe
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-02-04 19:20:07 +0330

    commit

--- src/app/components/Contaners.jsx
+++ src/app/components/Contaners.jsx
@@ -6,18 +6,14 @@ import Po from "./Po";
 export default function Contaners() {
   return (
     <div className=" flex flex-row  ">
-
       <div className=" bg-gray-100 sm:bg-gray-50">
-      <ParentListtTasks/>
+        <ParentListtTasks />
       </div>
-
-   
 
       <div className="bg-gray-100  w-[90%] ">
-      <ContanerCenter/>
+        <ContanerCenter />
       </div>
 
-     
       <div className=" bg-gray-100 sm:bg-gray-50 w-[5%]">left</div>
     </div>
 
